945b078dbfb56276e6cc82241598daa065cc03d8
Protobear <43970498+protobear@users.noreply.github.com>
2020-11-03 21:05:20 +0100

    added globals

--- scripts/code.js
+++ scripts/code.js
@@ -1,4 +1,26 @@
 //globals
+let backgroundImage;
+//now
+let nowDate;
+let nowImage;
+let nowTemp;
+// today
+let todayForecast;
+let todayImage;
+let todayMin;
+let todayMax;
+//tomorrow
+let tomorrowForecast;
+let tomorrowImage;
+let tomorrowMin;
+let tomorrowMax;
+//overmorrow
+let overmorrowTitle;
+let overmorrowForecast;
+let overmorrowImage;
+let overmorrowMin;
+let overmorrowMax;
+
 
 const setup = () => {
 
