ad4ca81b346e5285a37444c19af4db9af476f87f
protobear <43970498+protobear@users.noreply.github.com>
2020-11-17 23:35:33 +0100

    Only god knows what my error is, unfortunately i do not have enough rope available to go ask him.

--- scripts/code.js
+++ scripts/code.js
@@ -20,10 +20,41 @@ const setup = () => {
         if($("#searchInput").is(':focus') && e.which === 13) {
             globals.searchInputContent = $("#searchInput").val();
             console.log(globals.searchInputContent)
+            GetAndSetWeather();
         }
     });
 
 }
+
+const GetAndSetWeather = () =>{
+
+    currentURL = "http://api.weatherapi.com/v1/current.json?key=a9f26adcfd3d4e2fbda204136201611&q=Gent,BE";
+    //get and set current
+    $.ajax( {
+        url: currentURL,
+        //key: "a9f26adcfd3d4e2fbda204136201611",
+        type: "get",
+        success: setCurrentWeather,
+        error: myCodeIsACrime
+    })
+
+}
+
+const myCodeIsACrime = (err) =>{
+    console.log(err);
+}
+
+const setCurrentWeather = (receivedData) =>{
+    //now: nowdate, nowimage, nowtemp
+
+    let test = receivedData;
+    console.log(test);
+    $('nowTemp').val(receivedData.temp_c + " °C");
+
+
+}
+
+
 
 const getCurrent = () =>{
     let personInfo = $("#inputUsername").val();
@@ -36,21 +67,13 @@ const getCurrent = () =>{
         key: a9f26adcfd3d4e2fbda204136201611,
         type: "get",
         success: toPage,
-        error: nope
+        error: console.log("Something went wrong")
     })
     baseURL = "https://api.github.com/users/";
     currentURL = "http://api.weatherapi.com/v1/current.json?key=&q=Gent,BE";
     forecastURL = "http://api.weatherapi.com/v1/forecast.json?key=&q=Gent,BE&days=3";
 }
 
-const getForecast = () =>{
-
-}
-
-
-const topage = (dataFromServer) =>{
-
-}
 
 const nope = () =>{
     window.alert("Marnick made a fucky wucky and wants to grab the bottle again")
