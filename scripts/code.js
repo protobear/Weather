faded8d4b094784d3dccde433610b2173dc80861
protobear <43970498+protobear@users.noreply.github.com>
2020-12-19 14:40:29 +0100

    Finished

--- scripts/code.js
+++ scripts/code.js
@@ -2,12 +2,9 @@
 
 
 let globals = {
-    //not assigned by init
-    backgroundImage: '',
-    location: '',
 //Search
     searchInput: $("#searchInput"),
-    searchInputContent: ''
+    searchInputContent: '',
 }
 
 
@@ -15,44 +12,57 @@ let globals = {
 const setup = () => {
 
     $('#nowView').hide();
-    $('#nowBox').hover(()=>$('#nowView').show(), ()=> $('#nowView').hide());
-
     $('#todayView').hide();
+    $('#tomorrowView').hide();
+    $('#overmorrowView').hide();
+    $('#allW').hide();
+
+
+    $('#nowBox').hide();
+    $('#todayBox').hide();
+    $('#tomorrowBox').hide();
+    $('#overmorrowBox').hide();
+
+    $('#nowBox').hover(()=>$('#nowView').show(), ()=> $('#nowView').hide());
     $('#todayBox').hover(()=>$('#todayView').show(), ()=> $('#todayView').hide());
+    $('#tomorrowBox').hover(()=>$('#tomorrowView').show(), ()=> $('#tomorrowView').hide());
+    $('#overmorrowBox').hover(()=>$('#overmorrowView').show(), ()=> $('#overmorrowView').hide());
 
-
-
+    if($("#searchInput").is(':focus')){
+        $('#searchpop').show();
+    }
     //searchbar, if in focus and enter is pressed then searchinput is in searchinputcontent
     $(document).on('keypress',function(e) {
         if($("#searchInput").is(':focus') && e.which === 13) {
             globals.searchInputContent = $("#searchInput").val();
             console.log(globals.searchInputContent)
             GetAndSetWeather();
+            setInterval(GetAndSetWeather, 60000);
+            $('#searchpop').hide();
+            $('#allW').show();
+            changeBG();
+
+
         }
     });
 }
 
+const changeBG = () =>{
+
+    $("#bodyID").css("background-image", 'url("../Weather/assets/backgrounds/snow.jpg")');
+}
+
 
 const GetAndSetWeather = () =>{
-
-    currentURL = "http://api.weatherapi.com/v1/forecast.json?key=a9f26adcfd3d4e2fbda204136201611&q=Bornem&days=3"
+    currentURL = "http://api.weatherapi.com/v1/forecast.json?key=a9f26adcfd3d4e2fbda204136201611&q=" + globals.searchInputContent + "&days=3"
     //get and set current
     $.ajax( {
         url: currentURL,
-        //key: "a9f26adcfd3d4e2fbda204136201611",
         type: "get",
         success: setCurrentWeather
     })
 
 }
-
-const myCodeIsACrime = (jqXHR, textStatus, errorThrown) =>{
-    console.log("Error")
-    console.log(jqXHR);
-    console.log(textStatus);
-    console.log(errorThrown);
-}
-
 const setCurrentWeather = (receivedData) =>{
     //now: nowdate, nowimage, nowtemp
 
@@ -65,8 +75,8 @@ const setCurrentWeather = (receivedData) =>{
     $("#nowDate").text(data.location.localtime);
     $("#nowImage").attr("src", `https:${data.current.condition.icon}`);
 
-    $("#nowvisibility").text("//Visibility: " + data.current.vis_km +"KM");
-    $("#nowwindpeed").text("//Wind Speed: " + data.current.wind_kph +"KM/H");
+    $("#nowvisibility").text("//Visibility: " + data.current.vis_km +" KM");
+    $("#nowwindpeed").text("//Wind Speed: " + data.current.wind_kph +" KM/H");
 
 
 
@@ -76,8 +86,10 @@ const setCurrentWeather = (receivedData) =>{
     $("#todayMin").text(data.forecast.forecastday[0].day.mintemp_c + "°C");
     $("#todayMax").text(data.forecast.forecastday[0].day.maxtemp_c + "°C");
 
-    $("#todayvisibility").text("//Average visiblity" + data.forecast.forecastday[0].day.avgvis_km + "KM");
-    $("#todaywindpeed").text("//Average wind speed" + data.forecast.forecastday[0].day.maxwind_kph + "KM/H");
+    $("#todayvisibility").text("//Average Visibility: " + data.forecast.forecastday[0].day.avgvis_km + " KM");
+    $("#todaywindspeed").text("//Maximum Wind Speed: " + data.forecast.forecastday[0].day.maxwind_kph + " KM/H");
+
+
 
 
     //tomorrow
@@ -85,6 +97,10 @@ const setCurrentWeather = (receivedData) =>{
     $("#tomorrowImage").attr("src", `https:${data.forecast.forecastday[0].day.condition.icon}`);
     $("#tomorrowMin").text(data.forecast.forecastday[1].day.mintemp_c + "°C");
     $("#tomorrowMax").text(data.forecast.forecastday[1].day.maxtemp_c + "°C");
+
+
+    $("#tomorrowvisibility").text("//Average Visibility: " + data.forecast.forecastday[1].day.avgvis_km + " KM");
+    $("#tomorrowwindspeed").text("//Maximum Wind Speed: " + data.forecast.forecastday[1].day.maxwind_kph + " KM/H");
 
     //overmorrow (yes its a word i double checked)
 
@@ -98,6 +114,13 @@ const setCurrentWeather = (receivedData) =>{
     $("#overmorrowMin").text(data.forecast.forecastday[2].day.mintemp_c + "°C");
     $("#overmorrowMax").text(data.forecast.forecastday[2].day.maxtemp_c + "°C");
 
+    $("#overmorrowvisibility").text("//Average Visibility: " + data.forecast.forecastday[2].day.avgvis_km + " KM");
+    $("#overmorrowwindspeed").text("//Maximum Wind Speed: " + data.forecast.forecastday[2].day.maxwind_kph + " KM/H");
+
+    $('#nowBox').show();
+    $('#todayBox').show();
+    $('#tomorrowBox').show();
+    $('#overmorrowBox').show();
 }
 
 
