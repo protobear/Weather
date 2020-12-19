071aa7ae25ab7fa5481925e10cc878906bb4cc57
protobear <43970498+protobear@users.noreply.github.com>
2020-12-19 13:03:43 +0100

    nearly done

--- scripts/code.js
+++ scripts/code.js
@@ -13,7 +13,14 @@ let globals = {
 
 
 const setup = () => {
-// Assigning all the globals because I hate myself
+
+    $('#nowView').hide();
+    $('#nowBox').hover(()=>$('#nowView').show(), ()=> $('#nowView').hide());
+
+    $('#todayView').hide();
+    $('#todayBox').hover(()=>$('#todayView').show(), ()=> $('#todayView').hide());
+
+
 
     //searchbar, if in focus and enter is pressed then searchinput is in searchinputcontent
     $(document).on('keypress',function(e) {
@@ -23,12 +30,12 @@ const setup = () => {
             GetAndSetWeather();
         }
     });
-
 }
+
 
 const GetAndSetWeather = () =>{
 
-    currentURL = "http://api.weatherapi.com/v1/current.json?key=a9f26adcfd3d4e2fbda204136201611&q=Gent,BE";
+    currentURL = "http://api.weatherapi.com/v1/forecast.json?key=a9f26adcfd3d4e2fbda204136201611&q=Bornem&days=3"
     //get and set current
     $.ajax( {
         url: currentURL,
@@ -49,36 +56,47 @@ const myCodeIsACrime = (jqXHR, textStatus, errorThrown) =>{
 const setCurrentWeather = (receivedData) =>{
     //now: nowdate, nowimage, nowtemp
 
-    let test = receivedData;
-    console.log(test);
-    $('nowTemp').val(test.temp_c + " °C");
+    let data = receivedData;
+    console.log(data);
+
+    //current
+    $("#nowTemp").text(data.current.temp_c + "°C");
+    $("#nowTitle").text("//" + data.location.name);
+    $("#nowDate").text(data.location.localtime);
+    $("#nowImage").attr("src", `https:${data.current.condition.icon}`);
+
+    $("#nowvisibility").text("//Visibility: " + data.current.vis_km +"KM");
+    $("#nowwindpeed").text("//Wind Speed: " + data.current.wind_kph +"KM/H");
 
 
-}
+
+    //today
+    $("#todayForecast").text(data.forecast.forecastday[0].day.condition.text);
+    $("#todayImage").attr("src", `https:${data.forecast.forecastday[0].day.condition.icon}`);
+    $("#todayMin").text(data.forecast.forecastday[0].day.mintemp_c + "°C");
+    $("#todayMax").text(data.forecast.forecastday[0].day.maxtemp_c + "°C");
+
+    $("#todayvisibility").text("//Average visiblity" + data.forecast.forecastday[0].day.avgvis_km + "KM");
+    $("#todaywindpeed").text("//Average wind speed" + data.forecast.forecastday[0].day.maxwind_kph + "KM/H");
 
 
+    //tomorrow
+    $("#tomorrowForecast").text(data.forecast.forecastday[1].day.condition.text);
+    $("#tomorrowImage").attr("src", `https:${data.forecast.forecastday[0].day.condition.icon}`);
+    $("#tomorrowMin").text(data.forecast.forecastday[1].day.mintemp_c + "°C");
+    $("#tomorrowMax").text(data.forecast.forecastday[1].day.maxtemp_c + "°C");
 
-const getCurrent = () =>{
-    let personInfo = $("#inputUsername").val();
+    //overmorrow (yes its a word i double checked)
 
-    baseURL = baseURL.concat(personInfo);
-    console.log(baseURL);
-
-    $.ajax({
-        url: baseURL,
-        key: a9f26adcfd3d4e2fbda204136201611,
-        type: "get",
-        success: toPage,
-        error: console.log("Something went wrong")
-    })
-    baseURL = "https://api.github.com/users/";
-    currentURL = "http://api.weatherapi.com/v1/current.json?key=&q=Gent,BE";
-    forecastURL = "http://api.weatherapi.com/v1/forecast.json?key=&q=Gent,BE&days=3";
-}
+        let date = new Date(data.forecast.forecastday[2].date);
+        let datestring =  date.toLocaleDateString('en-US', { weekday: 'long' });
 
 
-const nope = () =>{
-    window.alert("Marnick made a fucky wucky and wants to grab the bottle again")
+    $("#overmorrowTitle").text("//" + datestring +"'s Forecast");
+    $("#overmorrowForecast").text(data.forecast.forecastday[2].day.condition.text);
+    $("#overmorrowImage").attr("src", `https:${data.forecast.forecastday[0].day.condition.icon}`);
+    $("#overmorrowMin").text(data.forecast.forecastday[2].day.mintemp_c + "°C");
+    $("#overmorrowMax").text(data.forecast.forecastday[2].day.maxtemp_c + "°C");
 
 }
 
